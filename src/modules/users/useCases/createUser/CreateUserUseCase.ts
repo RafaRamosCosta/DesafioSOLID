6486b84c63ca04e0e34e609adf489dee33a6a30d
= <=>
2022-12-20 10:21:59 -0300

    feat: add validatation if the email has already been registred.

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,6 +10,10 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
+    const user = this.usersRepository.findByEmail(email);
+    if (user.email === email)
+      throw new Error("This email has already been used!");
+
     return this.usersRepository.create({ name, email });
   }
 }
