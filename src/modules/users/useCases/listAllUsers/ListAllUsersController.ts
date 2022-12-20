553371088f29390a3a77c584bbb2b4c52267eb67
= <=>
2022-12-20 11:38:59 -0300

    feat: returning the users in the response.

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,9 +6,11 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.params;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const { user_id }: any = request.headers;
+
     const users = this.listAllUsersUseCase.execute({ user_id });
-    return response.status(200).json(users);
+    return response.status(200).json({ users });
   }
 }
 
